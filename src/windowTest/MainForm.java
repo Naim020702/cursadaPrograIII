700102c139ae7e488b9fd0ae02c10e8e3bee4b96
Naim21 <2002natorres@gmail.com>
2024-03-17 10:51:29 -0300

    Commit larva

--- src/windowTest/MainForm.java
+++ src/windowTest/MainForm.java
@@ -50,7 +50,7 @@ public class MainForm {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 414, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
